f51d000145b2ccf3135ed84e01558e3ac119da99
dhessmtr <dhess@mitre.org>
2025-09-17 19:18:31 +0000

    large git push

---
+++ docker-workstations/controls/example.rb
@@ -0,0 +1,12 @@
+# copyright: 2018, The Authors
+
+# describe yaml('docker-compose.yml') do
+#   its(['services', 'operatingsystem', 'image']) { should eq 'redhat/ubi9' }
+#   its(['services', 'nginx', 'image']) { should cmp 'nginx:latest' }
+# end
+
+describe docker_compose_config('docker-compose.yml') do
+  its('services.operatingsystem.image') { should eq 'redhat/ubi9' }
+  its('services.nginx.image') { should cmp 'nginx:latest' }
+end
+
